28996f0b7136352bb6a01b0643d1ac7439a6ba8b
YUVARAJC14 <129865133+YUVARAJC14@users.noreply.github.com>
2024-02-15 12:58:00 +0530

    Create Q1)Area_of_Circle.java

---
+++ Class_Test_1/Q1)Area_of_Circle.java
@@ -0,0 +1,7 @@
+public class AreaOfCircle{
+    public static void main(String[] args){
+        double r = 7.00;
+        double area = 3.14 * r * r;
+        System.out.print("The Area of Circle is: "+area);
+    }
+}
